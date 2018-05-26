f9c349309c841ded1081f9aeb7608b66e3ba27b2
Alexander_Malyavskiy <sasha.super1996@gmail.com>
2018-05-26 15:41:24 +0300

    recommit2

--- src/main/java/getGitLogin/DdlScript.java
+++ src/main/java/getGitLogin/DdlScript.java
@@ -3,16 +3,24 @@ package getGitLogin;
 import org.postgresql.util.PSQLException;
 
 import java.sql.*;
+import java.util.List;
 
 public class DdlScript {
+    private DBConnection dbConnection;
+    private Statement statement;
+
+    /**
+     * Instantiates a new Ddl script.
+     */
     public DdlScript() {
         try {
-            DBConnection dbConnection = new DBConnection();
-            Statement statement = dbConnection.getConnection().createStatement();
-            statement.execute("CREATE TABLE git_login(\n"
-                    + "login CHAR(64),\n"
-                    +"id INTEGER,\n"
-                    +"score REAL);");
+            dbConnection = new DBConnection();
+            if (!checkExists()){
+                statement.execute("CREATE TABLE git_login(\n"
+                        + "login CHAR(64),\n"
+                        +"id INTEGER,\n"
+                        +"score REAL);");
+            }
             statement.close();
             dbConnection.getConnection().close();
         } catch (PSQLException e) {
@@ -20,5 +28,16 @@ public class DdlScript {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean checkExists() throws SQLException {
+        statement = dbConnection.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from pg_tables where tablename !~'^pg'; ");
+        while (resultSet.next()){
+            if (resultSet.getString("tablename").equals("git_users")){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/getGitLogin/Main.java
+++ src/main/java/getGitLogin/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        DdlScript ddlScript = new DdlScript();
         if (args.length == 0){
             System.out.println("Error, to search for users, run the application with the parameters!");
         }
